0dd96e4359a3bd72c5abac8430ab481733b1f18f
Charles de MAGNEVAL <magneval@gmail.com>
2013-06-07 23:24:37 +0200

    Update demo.js

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,11 +1,1 @@
-window.addEvent('domready', function() {
-	new Request.HTML({
-		url: '/gh/get/response.html/magneval/jsFiddleGithubDemo/tree/master/Demo/',
-		data: {'delay': 1},
-		method: 'post',
-		update: 'demo',
-		onSuccess: function(response) {
-			$('demo').highlight();
-		}
-	}).send();
-})
+
